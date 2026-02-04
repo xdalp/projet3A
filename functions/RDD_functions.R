9326a778f2375f4bfc2351871042cfca8f3f2323
xdalp <mgarbe744@gmail.com>
2026-02-04 15:52:43 +0000

    R only on quarto

--- functions/RDD_functions.R
+++ functions/RDD_functions.R
@@ -1,9 +1,32 @@
-"""
-Ce fichier contient les fonctions en R pour l'économétrie et la dataviz
-"""
 library(dplyr)
 library(ggplot2)
 library(rdrobust)
+library(aws.s3)
+library(readr)
+
+get_s3_csv <- function(bucket,file_key) {
+  Sys.setenv(
+    "AWS_ACCESS_KEY_ID"     = Sys.getenv("AWS_ACCESS_KEY_ID"),
+    "AWS_SECRET_ACCESS_KEY" = Sys.getenv("AWS_SECRET_ACCESS_KEY"),
+    "AWS_SESSION_TOKEN"     = Sys.getenv("AWS_SESSION_TOKEN"),
+    "AWS_DEFAULT_REGION" = "us-east-1",
+    "AWS_S3_ENDPOINT"= "minio.lab.sspcloud.fr"
+  )
+  
+  # Lire le fichier CSV depuis S3
+  df <- s3read_using(
+    #FUN = readr::read_csv, # Fonction pour lire un CSV avec séparateur virgule
+    FUN = readr::read_delim, # Fonction pour choisir le séparateur du csv
+    delim = ";", # Spécifie le séparateur correct
+    object = file_key,
+    bucket = bucket,
+    opts = list("region" = "")
+  )
+  return(df)
+}
+
+
+
 
 compute_rdd <- function(df,
                         outcome_var,
